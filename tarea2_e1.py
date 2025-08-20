2040b94ff7dd1c96dab30684c334b2089be25526
SantiagoReyesC <alesantireyes2@gmail.com>
2025-08-20 16:13:44 -0600

    Add files via upload

---
+++ tarea2_e1.py
@@ -0,0 +1,7 @@
+x=3
+>>> sumat = 0
+>>> import math
+>>> for i in range(1,100|):
+...       sumat = sumat + ((x**i)/math.factorial(i))
+...
+>>> print(sumat)
---
+++ tarea2_e2.py
@@ -0,0 +1,8 @@
+x = 5
+>>> sumat = 0
+>>> import mat
+>>> for i in range(1,100):
+...       sumat = -sumat + (x**i)/i
+...
+>>> print(sumat)
+>>> print(math.log(1-x))
