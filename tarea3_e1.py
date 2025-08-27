0316638ecf975bba2d74f9638620da52e8e8c010
SantiagoReyesC <alesantireyes2@gmail.com>
2025-08-27 00:06:14 -0600

    Add files via upload

---
+++ tarea3_e1.py
@@ -0,0 +1,7 @@
+xx = text.replace('\n', ' ')
+comp = set (xx)
+
+for letra in list(comp):
+    if not (',' in letra or '.' in letra or '”' in letra): 
+     num = xx.count(letra)
+     print("frec de "+letra + "=" + str(num))
