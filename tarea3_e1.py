a0d40a1d9f74494cf5e0a76b7a8a78fdcdc1257f
SantiagoReyesC <alesantireyes2@gmail.com>
2025-08-27 00:07:02 -0600

    Update tarea3_e1.py

--- tarea3_e1.py
+++ tarea3_e1.py
@@ -1,7 +1,9 @@
+#Contar letras en un texto, variable tipo string
 xx = text.replace('\n', ' ')
 comp = set (xx)
 
 for letra in list(comp):
     if not (',' in letra or '.' in letra or '”' in letra): 
      num = xx.count(letra)
+
      print("frec de "+letra + "=" + str(num))
